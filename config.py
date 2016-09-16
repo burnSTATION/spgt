5452391a5d2ac96fd1da3cc6f903a6ec90f88263
Timothy Ng <timnjj@gmail.com>
2016-09-16 16:46:12 -0700

    Added some documentation to methods and variables

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # General Plugin Variables
-CHAT_PREFIX = "[\x02eSN\x01]" # This is included at the beginning of every chat message the plugin prints
-current_map = ""
+CHAT_PREFIX = "[\x02eSN\x01]"  # This is included at the beginning of every chat message the plugin prints
+current_map = ""  # The current map that the server is on
 
 # Pause Service Variables
-ct_pause_remaining = True # Starts the map with a counter terrorist pause granted
-t_pause_remaining = True # Starts the map with a terrorist pause granted
-pause_max_length = 90 # Maximum length that .pause can last
+ct_pause_remaining = True  # Starts the map with a counter terrorist pause granted
+t_pause_remaining = True  # Starts the map with a terrorist pause granted
+pause_max_length = 90  # Maximum length that .pause can last
 
 # Ready System Variables
-is_ready_period = True # Starts the server into a ready period
+is_ready_period = True  # Starts the server into a ready period
--- core_service.py
+++ core_service.py
@@ -7,6 +7,10 @@ from listeners.tick import Delay
 import spgt.config as config
 
 def get_map_name(map_name):
+    """ Given a map_name, checks for it against a "list" of dictionary keys. If
+    a match is found, return the value -- the map name accepted by the CSGO
+    console -- of said the matched key. If no match is found, return nothing.
+    """
     maps = {
     #   (aliases)                       : proper map name
         ("dust2", "de_dust2")           : "de_dust2",
@@ -27,11 +31,14 @@ def changemap(map_name):  # Callback function for Delay in .map
     engine_server.server_command('changelevel ' + map_name + ';')
 
 @OnLevelInit
-def on_level_init(map_name):
+def on_level_init(map_name):  # Update current_map on each map change
     config.current_map = map_name
 
 @SayCommand('.map')
 def map_command(command, index, team_only):
+    """ Allows the user to change the current map without using the CSGO
+    console. Syntax: '.map <map_name>'.
+    """
     try:
         user_input = command[1].lower()
         map = get_map_name(user_input)
@@ -42,7 +49,7 @@ def map_command(command, index, team_only):
             else:
                 SayText2(config.CHAT_PREFIX + " Current map is already " + map + ".").send()
         else:
-            SayText2(config.CHAT_PREFIX + " Invalid map name. Map pool: " +
+            SayText2(config.CHAT_PREFIX + " Invalid map name. Allowed maps: " +
                      "dust2, inferno, cache, train, mirage, " +
                      "cbbl, overpass").send()
 
